d7acf24227dbfb5d84b7a02753c77923ce1946b9
HoldYourWaffle <ravivanrooijen@live.nl>
2021-04-13 03:29:35 +0200

    Put both IPluginLoader interfaces in the same file

--- src/Kontract/Interfaces/Loaders/IPluginLoader.cs
+++ src/Kontract/Interfaces/Loaders/IPluginLoader.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Kontract.Interfaces.Plugins.Identifier;
 using Kontract.Models;
 
 namespace Kontract.Interfaces.Loaders
@@ -22,5 +23,24 @@ namespace Kontract.Interfaces.Loaders
         /// <param name="pluginId">The unique Id of a plugin.</param>
         /// <returns>If the plugin exists in this instance.</returns>
         bool Exists(Guid pluginId);
+    }
+    
+    /// <summary>
+    /// Exposes methods to handle a specific type of plugin.
+    /// </summary>
+    /// <typeparam name="TPlugin">The type of the plugin to retrieve.</typeparam>
+    public interface IPluginLoader<out TPlugin> : IPluginLoader where TPlugin : IPlugin
+    {
+        /// <summary>
+        /// A read-only list of plugins loaded by this instance.
+        /// </summary>
+        IReadOnlyList<TPlugin> Plugins { get; }
+
+        /// <summary>
+        /// Gets a plugin from this instance.
+        /// </summary>
+        /// <param name="pluginId">The unique Id of a plugin.</param>
+        /// <returns>The plugin with the given unique Id.</returns>
+        TPlugin GetPlugin(Guid pluginId);
     }
 }
